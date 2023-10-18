6ea72860d62cd15ae48fd149a117aea075b7d3aa
Filma Bem <filmab.emtv2@gmail.com>
2023-10-18 17:10:39 +0100

    Added a new enderio recipe

--- kubejs/server_scripts/enderio_machines/fire_crafting.js
+++ kubejs/server_scripts/enderio_machines/fire_crafting.js
@@ -1,6 +1,8 @@
 
 ServerEvents.recipes(event => { 
 
+    // Grains of infinity
+
     event.custom({
         type: 'enderio:fire_crafting',
         base_blocks: [
@@ -14,6 +16,24 @@ ServerEvents.recipes(event => {
         ],
         loot_table: 'enderio:fire_crafting/infinity',
         max_item_drops: '2'
+    })
+
+    // Luminessence
+
+    event.custom({
+        type: 'enderio:fire_crafting',
+        base_blocks: [
+            {
+                block: 'minecraft:obsidian'
+            }
+        ],
+        dimensions: [
+            'minecraft:overworld',
+            'minecraft:nether',
+            'minecraft:the_end'
+        ],
+        loot_table: 'se5:fire_crafting/luminessence',
+        max_item_drops: '5'
     })
 
 })
