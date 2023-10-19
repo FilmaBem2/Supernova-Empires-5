58add3226918a08ee2e0b45525d6ae5942d0249a
Filma Bem <filmab.emtv2@gmail.com>
2023-10-19 16:46:41 +0100

    Added the minerable tag to fbmodium blocks

--- kubejs/startup_scripts/se5/blocks/blocks.js
+++ kubejs/startup_scripts/se5/blocks/blocks.js
@@ -3,9 +3,9 @@ StartupEvents.registry("block", event => {
 
     // Fbmodium
 
-    event.create('fbmodium_block').material('glass').hardness(8.0).displayName('Fbmodium Block').requiresTool(true)
-    event.create('nether_infused_fbmodium_block').material('glass').hardness(8.0).displayName('Nether Infused Fbmodium Block').requiresTool(true)
-    event.create('end_infused_fbmodium_block').material('glass').hardness(8.0).displayName('End Infused Fbmodium Block').requiresTool(true)
-    event.create('ultimate_infused_fbmodium_block').material('glass').hardness(8.0).displayName('Ultimate Infused Fbmodium Block').requiresTool(true)
+    event.create('fbmodium_block').material('glass').hardness(8.0).displayName('Fbmodium Block').requiresTool(true).tagBlock('minecraft:minerable/pickaxe')
+    event.create('nether_infused_fbmodium_block').material('glass').hardness(8.0).displayName('Nether Infused Fbmodium Block').requiresTool(true).tagBlock('minecraft:minerable/pickaxe')
+    event.create('end_infused_fbmodium_block').material('glass').hardness(8.0).displayName('End Infused Fbmodium Block').requiresTool(true).tagBlock('minecraft:minerable/pickaxe')
+    event.create('ultimate_infused_fbmodium_block').material('glass').hardness(8.0).displayName('Ultimate Infused Fbmodium Block').requiresTool(true).tagBlock('minecraft:minerable/pickaxe')
 
   })
