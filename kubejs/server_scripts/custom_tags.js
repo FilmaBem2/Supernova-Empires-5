afcdba0da6b5a19ea2458a9d406f606cf396a498
FilmaBem2 <filmab.emtv2@gmail.com>
2023-10-21 16:27:57 +0100

    Added tags for fbmodium ingots

---
+++ kubejs/server_scripts/custom_tags.js
@@ -0,0 +1,19 @@
+
+ServerEvents.tags('item', event => {
+
+    // Forge global ingots
+    
+    event.add('forge:ingots', 'kubejs:fbmodium_ingot')
+    event.add('forge:ingots', 'kubejs:nether_infused_fbmodium_ingot')
+    event.add('forge:ingots', 'kubejs:end_infused_fbmodium_ingot')
+    event.add('forge:ingots', 'kubejs:ultimate_infused_fbmodium_ingot')
+
+    // Forge Specific Ingots
+
+    event.add('forge:ingots/fbmodium', 'kubejs:fbmodium_ingot')
+    event.add('forge:ingots/nether_fbmodium', 'kubejs:nether_infused_fbmodium_ingot')
+    event.add('forge:ingots/end_fbmodium', 'kubejs:end_infused_fbmodium_ingot')
+    event.add('forge:ingots/ultimate_fbmodium', 'kubejs:ultimate_infused_fbmodium_ingot')
+    
+
+  })
