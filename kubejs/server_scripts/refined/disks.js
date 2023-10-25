e8761cb936994b63e33ab5f612b0f9ed9ce80953
FilmaBem2 <filmab.emtv2@gmail.com>
2023-10-25 02:58:37 +0100

    Fixed refined storage recipe bugs

--- kubejs/server_scripts/refined/disks.js
+++ kubejs/server_scripts/refined/disks.js
@@ -19,256 +19,134 @@ ServerEvents.recipes(event => {
             }
         })
     }
-    
+
+    let basic_disk_part = (output, input) => {
+        event.custom({
+            type: 'extendedcrafting:shaped_flux_crafter',
+            powerRequired: 100000,
+            powerRate: 400,
+            pattern: [
+                'ABA',
+                'CDC',
+                'ACA'
+            ],
+            key: {
+                A: {
+                    tag: 'forge:silicon'
+                },
+                B: {
+                    item: 'refinedstorage:quartz_enriched_iron'
+                },
+                C: {
+                    tag: 'forge:glass'
+                },
+                D: {
+                    item: input
+                }
+            },
+            result: {
+                item: output
+            }
+        })
+    }
+
+    let advanced_disk_part = (output, part, processor, type) => {
+        event.custom({
+            type: 'extendedcrafting:shaped_flux_crafter',
+            powerRequired: 100000,
+            powerRate: 400,
+            pattern: [
+                'ABA',
+                'CDC',
+                'ACA'
+            ],
+            key: {
+                A: {
+                    item: processor
+                },
+                B: {
+                    item: 'refinedstorage:quartz_enriched_iron'
+                },
+                C: {
+                    item: part
+                },
+                D: {
+                    item: type
+                }
+            },
+            result: {
+                item: output
+            }
+        })
+    }
+
+    let disks = (output, part) => {
+        event.custom({
+            type: 'extendedcrafting:shaped_flux_crafter',
+            powerRequired: 100000,
+            powerRate: 400,
+            pattern: [
+                'ABA',
+                'BCB',
+                'DDD'
+            ],
+            key: {
+                A: {
+                    tag: 'forge:glass'
+                },
+                B: {
+                    item: 'minecraft:redstone'
+                },
+                C: {
+                    item: part
+                },
+                D: {
+                    item: 'refinedstorage:quartz_enriched_iron'
+                }
+            },
+            result: {
+                item: output
+            }
+        })
+    }
+
     // --- Disk Parts ---
 
     // -- Storage --
 
     // 1K
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                tag: 'forge:silicon'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                tag: 'forge:glass'
-            },
-            D: {
-                item: 'minecraft:redstone'
-            }
-        },
-        result: {
-            item: 'refinedstorage:1k_storage_part'
-        }
-    })
-
+    basic_disk_part('refinedstorage:1k_storage_part', 'minecraft:redstone')
 
     // 4k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                item: 'refinedstorage:basic_processor'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                item: 'refinedstorage:1k_storage_part'
-            },
-            D: {
-                item: 'minecraft:redstone'
-            }
-        },
-        result: {
-            item: 'refinedstorage:4k_storage_part'
-        }
-    })
-
+    advanced_disk_part('refinedstorage:4k_storage_part', 'refinedstorage:1k_storage_part', 'refinedstorage:basic_processor', 'minecraft:redstone')
 
     // 16k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                item: 'refinedstorage:improved_processor'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                item: 'refinedstorage:4k_storage_part'
-            },
-            D: {
-                item: 'minecraft:redstone'
-            }
-        },
-        result: {
-            item: 'refinedstorage:16k_storage_part'
-        }
-    })
-
+    advanced_disk_part('refinedstorage:16k_storage_part', 'refinedstorage:4k_storage_part', 'refinedstorage:improved_processor', 'minecraft:redstone')
 
     // 64k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                item: 'refinedstorage:advanced_processor'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                item: 'refinedstorage:16k_storage_part'
-            },
-            D: {
-                item: 'minecraft:redstone'
-            }
-        },
-        result: {
-            item: 'refinedstorage:64k_storage_part'
-        }
-    })
-
-
+    advanced_disk_part('refinedstorage:64k_storage_part', 'refinedstorage:16k_storage_part', 'refinedstorage:advanced_processor', 'minecraft:redstone')
+    
     // -- Fuild --
 
     // 64k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                tag: 'forge:silicon'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                tag: 'forge:glass'
-            },
-            D: {
-                item: 'minecraft:bucket'
-            }
-        },
-        result: {
-            item: 'refinedstorage:64k_fluid_storage_part'
-        }
-    })
+    basic_disk_part('refinedstorage:64k_fluid_storage_part', 'minecraft:bucket')
 
     // 256k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                item: 'refinedstoragen:basic_processor'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                item: 'refinedstorage:64k_fluid_storage_part'
-            },
-            D: {
-                item: 'minecraft:bucket'
-            }
-        },
-        result: {
-            item: 'refinedstorage:256k_fluid_part'
-        }
-    })
+    advanced_disk_part('refinedstorage:256k_fluid_storage_part', 'refinedstorage:64k_fluid_storage_part', 'refinedstorage:basic_processor', 'minecraft:bucket')
 
     // 1024k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                item: 'refinedstoragen:improved_processor'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                item: 'refinedstorage:256k_fluid_storage_part'
-            },
-            D: {
-                item: 'minecraft:bucket'
-            }
-        },
-        result: {
-            item: 'refinedstorage:1024k_fluid_part'
-        }
-    })
-
+    advanced_disk_part('refinedstorage:1024k_fluid_storage_part', 'refinedstorage:256k_fluid_storage_part', 'refinedstorage:improved_processor', 'minecraft:bucket')
+    
     // 4096
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'CDC',
-            'ACA'
-        ],
-        key: {
-            A: {
-                item: 'refinedstoragen:advanced_processor'
-            },
-            B: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            },
-            C: {
-                item: 'refinedstorage:1024k_fluid_storage_part'
-            },
-            D: {
-                item: 'minecraft:bucket'
-            }
-        },
-        result: {
-            item: 'refinedstorage:4096k_fluid_part'
-        }
-    })
+    advanced_disk_part('refinedstorage:4096k_fluid_storage_part', 'refinedstorage:1024k_fluid_storage_part', 'refinedstorage:advanced_processor', 'minecraft:bucket')
 
     // --- Disks ---
 
@@ -276,260 +154,44 @@ ServerEvents.recipes(event => {
 
     // 1k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:1k_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:1k_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:1k_storage_disk', 'refinedstorage:1k_storage_part')
     disk_from_housing('refinedstorage:1k_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:1k_storage_part')
 
     // 4k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:4k_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:4k_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:4k_storage_disk', 'refinedstorage:4k_storage_part')
     disk_from_housing('refinedstorage:4k_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:4k_storage_part')
 
     // 16k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:16k_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:16k_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:16k_storage_disk', 'refinedstorage:16k_storage_part')
     disk_from_housing('refinedstorage:16k_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:16k_storage_part')
 
     // 64k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:64k_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:64k_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:64k_storage_disk', 'refinedstorage:64k_storage_part')
     disk_from_housing('refinedstorage:64k_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:64k_storage_part')
 
     // -- Fluid --
 
     // 64k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:64k_fluid_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:64k_fluid_storage_disk'
-        }
-    })
-
-    disk_from_housing('refinedstorage:64k_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:64k_storage_part')
+    disks('refinedstorage:64k_fluid_storage_disk', 'refinedstorage:64k_fluid_storage_part')
+    disk_from_housing('refinedstorage:64k_fluid_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:64k_fluid_storage_part')
 
     // 256k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:256k_fluid_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:256k_fluid_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:256k_fluid_storage_disk', 'refinedstorage:256k_fluid_storage_part')
     disk_from_housing('refinedstorage:256k_fluid_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:256k_fluid_storage_part')
 
     // 1024k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:1024k_fluid_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:1024k_fluid_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:1024k_fluid_storage_disk', 'refinedstorage:1024k_fluid_storage_part')
     disk_from_housing('refinedstorage:1024k_fluid_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:1024k_fluid_storage_part')
 
     // 4096k
 
-    event.custom({
-        type: 'extendedcrafting:shaped_flux_crafter',
-        powerRequired: 100000,
-        powerRate: 400,
-        pattern: [
-            'ABA',
-            'BCB',
-            'DDD'
-        ],
-        key: {
-            A: {
-                tag: 'forge:glass'
-            },
-            B: {
-                item: 'minecraft:redstone'
-            },
-            C: {
-                item: 'refinedstorage:4096k_fluid_storage_part'
-            },
-            D: {
-                item: 'refinedstorage:quartz_enriched_iron'
-            }
-        },
-        result: {
-            item: 'refinedstorage:4096k_fluid_storage_disk'
-        }
-    })
-
+    disks('refinedstorage:4096k_fluid_storage_disk', 'refinedstorage:4096k_fluid_storage_part')
     disk_from_housing('refinedstorage:4096k_fluid_storage_disk', 'refinedstorage:storage_housing', 'refinedstorage:4096k_fluid_storage_part')
 
 })
