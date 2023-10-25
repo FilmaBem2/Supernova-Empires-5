913a7665d1727d9a1f87625f26f7a00315abc197
FilmaBem2 <filmab.emtv2@gmail.com>
2023-10-25 01:46:38 +0100

    Worldgen things for fbmodium ores when it's available in kubejs for minecraft 1.20

---
+++ kubejs/startup_scripts/worldgen.js
@@ -0,0 +1,15 @@
+
+// WorldgenEvents.add(event => {
+
+//     // Fbmodium Ore
+
+//     event.addOre(ore => {
+
+//         ore.id = 'kubejs:fbmodium_ore'
+//         ore.biomes = { not: 'minecraft:ocean' }	// spawn in all biomes that are NOT 'minecraft:ocean'
+//         ore.addTarget('#minecraft:stone_ore_replaceables', 'kubejs:fbmodium_ore')
+//         ore.addTarget('minecraft:deepslate', 'kubejs:deepslate_fbmodium_ore')
+//         ore.count([1, 3]).squared()
+
+//     })
+//   })
